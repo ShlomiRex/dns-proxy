b685b4b1937f6d54b643d317d9039329fc26acee
Yitzchak <hmje.sh@gmail.com>
2019-09-01 11:58:54 +0300

    z

--- proxy.py
+++ proxy.py
@@ -43,22 +43,6 @@ def print_udp_pkt(pkt):#
 	print "\n"
 
 def dns_sniff(pkt):
-	if pkt.haslayer(DNS) == False or pkt.haslayer(DNSRR) == False:
-		return
-
-	ip_src = pkt[IP].src
-	ip_dst = pkt[IP].dst
-
-	port_src = pkt[UDP].sport
-	port_dst = pkt[UDP].dport
-
-	#print "dst = " +str(port_dst)
-
-	if port_dst != PORT_SERVER2PROXY:
-		return
-	
-	print "DNS Response"
-	print "-----------------------------"
 	print_udp_pkt(pkt)
 
 
@@ -67,20 +51,25 @@ def dns_sniff(pkt):
 
 
 def print_console():
-    print "[0] <exit>"
-    print "[1] Google"
-    print "[2] YouTube"
+	print "[0] <exit>"
+	print "[1] Google"
+	print "[2] YouTube"
 
 def wait4answers():
 	print "[wait4answers] Running..."
-	filter = "ip and udp"
+	filter = "udp and port 53 and dst port 53000"
+	print "DNS Response"
+	print "-----------------------------"
 	sniff(prn=dns_sniff, filter=filter, timeout=WAIT4ANSWERS_SECONDS)
 	print "[wait4answers] Done"
 
 
 while True:
-    print_console()
-    num = input("Enter your function number to run: ")
+	#print_console()
+	getAnswers()
+	print "\n\n"
+	num = input("Enter your function number to run: ")
+
     if num == 0:
         exit()
     elif num == 1:
@@ -129,3 +118,14 @@ class Wait4Answers(Thread):
 
 		self.prints(str(ip_src) + ":" + str(port_src) + " -> " + str(ip_dst) + ":" + str(port_dst))
 '''
+
+
+answers_time = 0   # If it goes above WAIT4ANSWERS_MS then stop gathering answers
+answers_cache = []
+filterPkt = "udp and port 53 and dst port 53000"
+packets=[]
+def addAnswer(pkt):
+	packets.append(pkt)
+def getAnswers():
+	pppp = sniff(prn = addAnswer, timeout = 3)
+	print type(pppp)
