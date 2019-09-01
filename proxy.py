f22aec7cde0abc4cccba3edae0c93951c62a9f47
Yitzchak <hmje.sh@gmail.com>
2019-09-01 14:19:51 +0300

    analyze

--- proxy.py
+++ proxy.py
@@ -15,7 +15,7 @@ PORT_SERVER_DNS = 53
 DEBUG_SERVER2PROXY = True
 DEBUG_WAIT4ANSWERS = False
 
-WAIT4ANSWERS_SECONDS = 2        #time to wait to allow time to gather multiple answers
+WAIT4ANSWERS_SECONDS = 4        #time to wait to allow time to gather multiple answers
 REQUEST_WAIT_FOR_THREAD = 0.2
 
 
@@ -29,7 +29,6 @@ def print_udp_pkt(pkt):#
 	port_dst = pkt[UDP].dport
 
 	print str(ip_src) + ":" + str(port_src) + " -> " + str(ip_dst) + ":" + str(port_dst)
-	print "\n"
 
 def dns_sniff(pkt):
 	print_udp_pkt(pkt)
@@ -54,14 +53,30 @@ def wait4answers():
 	sniff(prn=dns_sniff, filter=filter, timeout=WAIT4ANSWERS_SECONDS)
 	print "[wait4answers] Done"
 
+def analyze():
+	ans = []
+	if(len(answers_cache)>0):
+		for x in range(answers_cache[0][DNS].ancount):
+			ans.append(answers_cache[0][DNSRR][x].rdata)
+		for i in range (1, len(answers_cache)):
+			for x in range(answers_cache[i][DNS].ancount):
+				if (not answers_cache[i][DNSRR][x].rdata in ans):
+					print "!!!!!!!!!!!!!"
+				else:
+					print "good"
+
 def begin(query_name):
 		thread = Thread(target = wait4answers)
 		thread.start()
 		time.sleep(REQUEST_WAIT_FOR_THREAD)
 		dns_req = IP(dst=IP_DNS_SERVER)/UDP(sport=PORT_SERVER2PROXY, dport=PORT_SERVER_DNS)/DNS(rd=1, qd=DNSQR(qname=query_name))
-		send(dns_req)
+		send(dns_req, count = 2)
 		thread.join()
-		print "# of Answers got: " + str(len(answers_cache))
+		print "# of Answers got: " + str(len(answers_cache))+"\n\n"
+		print "analyze answers:"
+
+		analyze()
+				
 		global answers_cache
 		answers_cache = []
 		
@@ -77,5 +92,3 @@ while True:
 
 	elif num == 2:
 		begin("www.youtube.com")
-
-	print "\n\n"
